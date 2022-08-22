973d9085b1c415dd2273f62503a540182522c727
sgtmariebiscuit <sgt.mariebiscuit@gmail.com>
2022-08-22 13:02:11 +0200

    Solved Divisibility

---
+++ 004_HackerEarth_Divisibility/004_HackerEarth_Divisibility/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_HackerEarth_Divisibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = Int32.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split(' ');
+            string ans = input[input.Length - 1];
+
+            if (ans[ans.Length - 1] == '0')
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
