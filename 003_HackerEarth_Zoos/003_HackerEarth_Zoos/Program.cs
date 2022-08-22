e32f7f412a2d8357a3fb3e4733e78055679d2fad
sgtmariebiscuit <sgt.mariebiscuit@gmail.com>
2022-08-22 12:19:57 +0200

    Solved zoos

---
+++ 003_HackerEarth_Zoos/003_HackerEarth_Zoos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_HackerEarth_Zoos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            decimal charZ = 0;
+            decimal charO = 0;
+
+            foreach (char c in input)
+            {
+                if (c == 'z')
+                {
+                    charZ++;
+                }
+                else if (c == 'o')
+                {
+                    charO++;
+                }
+            }
+
+            if (charO / charZ == 2)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
+
